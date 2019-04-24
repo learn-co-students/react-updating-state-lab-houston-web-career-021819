4f0033b47996689f46c1cd1b0a5deb27fc0132b4
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-04-24 17:48:56 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+export default class DigitalClicker extends Component{
+    state = {timesClicked:0}
+    
+    newCount = ()=>{ 
+       const num=this.state.timesClicked + 1
+       this.setState({timesClicked:num})
+    }
+    render(){
+        return(
+      <div>
+     <button onClick = {this.newCount}>{this.state.timesClicked}</button>
+     </div>
+        )
+     
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,33 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+export default class YouTubeDebugger extends Component{
+state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+
+  changeBitRate=()=>{
+      this.setState({settings:
+     {...this.state.settings,
+      bitrate: 12}})
+     }
+  changeResolution=()=>{
+      this.setState({...this.state,settings:{...this.state.settings, video:{resolution:'720p'}
+    }
+    })
+  }
+  render(){
+      return(
+          <div>
+          <button className='bitrate' onClick={this.changeBitRate}>Change Bit Rate</button>
+          <button className = 'resolution' onClick={this.changeResolution}>Change Resolution</button>
+          </div>
+      )
+  }
+}
